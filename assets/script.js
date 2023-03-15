03d576988319dd8a69a25dd4a721859a1b73d5e1
cdverdugo <109557242+cdverdugo@users.noreply.github.com>
2023-03-15 16:28:28 +0000

    nomob

--- assets/script.js
+++ assets/script.js
@@ -26,28 +26,12 @@ function showSlides(n) {
   slides[slideIndex-1].style.display = "block";
   dots[slideIndex-1].className += " active";
 }
+/* Toggle between showing and hiding the navigation menu links when the user clicks on the hamburger menu / bar icon */
 function myFunction() {
-  var x = document.getElementById("myLinks");
+  var x = document.getElementById("myDropdown");
   if (x.style.display === "block") {
     x.style.display = "none";
   } else {
     x.style.display = "block";
   }
-}
-/* Set the width of the side navigation to 250px and the left margin of the page content to 250px and add a black background color to body */
-function openNav() {
-  document.getElementById("mySidenav").style.width = "125px";
-  document.getElementById("main").style.marginLeft = "0px";
-  document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
-  
-}
-
-/* Set the width of the side navigation to 0 and the left margin of the page content to 0, and the background color of body to white */
-function closeNav() {
-  document.getElementById("mySidenav").style.width = "0";
-  document.getElementById("main").style.marginLeft = "0";
-  document.body.style.backgroundColor = "white";
-}
-function myFunction(x) {
-  x.classList.toggle("change");
 }
---
+++ here.js
@@ -0,0 +1,1 @@
+<button onclick="showAlert()">Show alert</button>
